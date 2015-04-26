f7c045b41ca75f7ae78f8db2323bf16031a2956e
KingofGamesYami <dirthsj@mcsdonline.org>
2015-04-25 21:47:42 -0500

    Comments
    Added lots o' comments, as requested!

--- src/Turtle/lamaedit.lua
+++ src/Turtle/lamaedit.lua
@@ -1,23 +1,26 @@
+--Copy the default turtle directory
 local turtle = {}
 for k, v in pairs( _G.turtle ) do
 	turtle[ k ] = v
 end
 
+--get the current env
+--anything declared as env.name will end up in the loaded API
 local env = getfenv()
 
 --Fuel tracking
 local fuel = {}
-fuel.load = function()
-	if fs.exists( ".fuel" ) then
+fuel.load = function() --loading fuel data
+	if fs.exists( ".fuel" ) then --if we've got previous data, we want to use it
 		local file = fs.open( ".fuel", "r" )
 		fuel.amount = tonumber( file.readAll() )
 		file.close()
-	else
+	else --otherwise, use the current fuel level
 		fuel.amount = turtle.getFuelLevel()
 	end
 end
 
-fuel.save = function()
+fuel.save = function() --save fuel data
 	local file = fs.open( ".fuel", "w" )
 	file.write( fuel.amount )
 	file.close()
@@ -25,7 +28,7 @@ end
 
 --facing tracking
 local facing = {}
-facing.turnRight = function()
+facing.turnRight = function() --changes the facing clockwise (on a compass) once
 	if facing.face == "north" then
 		facing.face = "east"
 	elseif facing.face == "east" then
@@ -37,44 +40,44 @@ facing.turnRight = function()
 	end
 end
 
-facing.save = function()
+facing.save = function() --saves facing and current movement direction
 	local file = fs.open( ".facing", "w" )
 	file.write( textutils.serialize( {facing.face, facing.direction} ) )
 	file.close()
 end
 
-facing.load = function()
-	if fs.exists( ".facing" ) then
+facing.load = function() --loads facing / current movement direction
+	if fs.exists( ".facing" ) then --if we have previous data, we use it
 		local file = fs.open( ".facing", "r" )
 		facing.face, facing.direction = unpack( textutils.unserialize( file.readAll() ) )
 		file.close()
-	else
+	else --otherwise, turtle defaults to facing north (direction does not need to be preset)
 		facing.face = "north"
 	end
 end
 
 --position tracking
 local position = {}
-position.save = function()
-	position.update()
+position.save = function() --saves position (x, y, z)
+	position.update() --update the position based on direction and fuel level, then save it to a file
 	local file = fs.open( ".position", "w" )
 	file.write( textutils.serialize( { position.x, position.y, position.z } ) )
 	file.close()
 end
 
-position.load = function()
-	if fs.exists( ".position" ) then
+position.load = function() --loads position (x, y z)
+	if fs.exists( ".position" ) then --if we have previous data, use it
 		local file = fs.open( ".position", "r" )
-		position.x, position.y, position.z = unpack( textutils.unserialize( file.readAll():match("{.-}$") ) )
+		position.x, position.y, position.z = unpack( textutils.unserialize( file.readAll() ) )
 		file.close()
-	else
+	else --otherwise assume we are at 1, 1, 1
 		position.x, position.y, position.z = 1, 1, 1
 	end
 end
 
-position.update = function()
+position.update = function() --updates the position of the turtle
 	local diff = fuel.amount - turtle.getFuelLevel()
-	if diff > 0 then
+	if diff > 0 then --if we've spent fuel (ei moved), we'll need to move that number in a direction
 		if facing.direction == 'north' then
 			position.x = position.x + diff
 		elseif facing.direction == "south" then
@@ -89,9 +92,11 @@ position.update = function()
 			position.y = position.y - diff
 		end
 	end
-	fuel.amount = turtle.getFuelLevel()
+	fuel.amount = turtle.getFuelLevel() --update the fuel amount
+	fuel.save() --save the fuel amount
 end
 
+--direct opposite compass values, mainly for env.back
 local opposite = {
 	["north"] = "south",
 	["south"] = "north",
@@ -99,16 +104,16 @@ local opposite = {
 	["west"] = "east",
 }
 
-env.forward = function()
-	if facing.direction ~= facing.face then
-		position.save()
-		facing.direction = facing.face
-		facing.save()
+env.forward = function() --basically, turtle.forward
+	if facing.direction ~= facing.face then --if we were going a different direction before
+		position.save() --save out position
+		facing.direction = facing.face --update the direction
+		facing.save() --save the direction
 	end
-	return turtle.forward()
+	return turtle.forward() --go forward, return result
 end
 
-env.back = function()
+env.back = function() --same as env.forward, but going backwards
 	if facing.direction ~= opposite[ facing.face ] then
 		position.save()
 		facing.direction = opposite[ facing.face ]
@@ -117,16 +122,16 @@ env.back = function()
 	return turtle.back()
 end
 
-env.up = function()
-	if facing.direction ~= "up" then
-		position.save()
-		facing.direction = "up"
-		facing.save()
+env.up = function() --turtle.up
+	if facing.direction ~= "up" then --if we were going a different direction
+		position.save() --save our position
+		facing.direction = "up" --set the direction to up
+		facing.save() --save the direction
 	end
-	return turtle.up()
+	return turtle.up() --go up, return result
 end
 
-env.down = function()
+env.down = function() --env.up, but for going down
 	if facing.direction ~= "down" then
 		position.save()
 		facing.direction = "down"
@@ -135,54 +140,54 @@ env.down = function()
 	return turtle.down()
 end
 
-env.turnRight = function()
+env.turnRight = function() --turtle.turnRight
+	position.save() --save the position (x,y,z)
+	facing.turnRight() --update our compass direction
+	facing.save() --save it
+	return turtle.turnRight() --return the result
+end
+
+env.turnLeft = function() --env.turnRight, but the other direction
 	position.save()
+	facing.turnRight() --going clockwise 3 times is the same as
+	facing.turnRight() --going counterclockwise once
 	facing.turnRight()
 	facing.save()
 	return turtle.turnRight()
 end
 
-env.turnLeft = function()
-	position.save()
-	facing.turnRight()
-	facing.turnRight()
-	facing.turnRight()
-	facing.save()
-	return turtle.turnRight()
-end
-
-env.refuel = function( n )
-	position.update()
-	if turtle.refuel( n ) then
-		fuel.amount = turtle.getFuelLevel()
-		fuel.save()
+env.refuel = function( n ) --needed because we depend on fuel level
+	position.update() --update our position
+	if turtle.refuel( n ) then --if we refueled then
+		fuel.amount = turtle.getFuelLevel() --set our amount to the current level
+		fuel.save() --save that amount
 		return true
 	end
-	return false
+	return false --otherwise, return false
 end
 
-env.overwrite = function( t )
-	t = t or _G.turtle
+env.overwrite = function( t ) --writes env values into the table given
+	t = t or _G.turtle    --or, if no value was given, _G.turtle
 	for k, v in pairs( env ) do
 		t[ k ] = v
 	end
 end
 
-env.getPosition = function()
-	position.update()
+env.getPosition = function() --returns the current position of the turtle
+	position.update() --first we should update the position (otherwise it'll give coords of the last time we did this)
 	return position.x, position.y, position.z, facing.face
 end
 
-env.setPosition = function( x, y, z, face )
+env.setPosition = function( x, y, z, face ) --sets the current position of the turtle
 	position.x = x
 	position.y = y
 	position.z = z
-	facing.face = face or facing.face
-	position.save()
-	facing.save()
+	facing.face = face or facing.face --default the the current facing if it's not provided
+	position.save() --save our new position
+	facing.save() --save the way we are facing
 end
 
-facing.load()
+facing.load() --just loading stuff
 position.load()
 fuel.load()
-position.update()
+position.update() --and updating our coords
