67a2834652571ef253d9e605c848fdfb1bac0543
KingofGamesYami <dirthsj@mcsdonline.org>
2015-05-04 07:35:46 -0500

    Update newInstaller.lua
    +not downloading installer
    +only downloading lua files
    +minor changes to download functions

--- src/Shared/newInstaller.lua
+++ src/Shared/newInstaller.lua
@@ -29,7 +29,10 @@ do
 end
 
 local dirIgnore = {} --dirs to ignore (blacklist)
-local fileIgnore = {} --Files to ignore (blacklist)
+local fileIgnore = { --Files to ignore (blacklist)
+	["Shared/installer.lua"] = true,
+	["Shared/newInstaller.lua"] = true,
+}
 
 if turtle then --setting up blacklist
 	dirIgnore[ "Pocket" ] = true
@@ -46,20 +49,21 @@ end
 
 local dirsToGet = {"https://api.github.com/repos/lupus590/CC-Hive/contents/src"} --a table of directories to download & scan
 local filesToGet = {} --a table of files we need to download
+local isRunning = true
 
-local function scanDir( response ) --a function for scanning a directory and allocating it's contents to the above tables
-	local f = json.decode( response.readAll() )
+local function scanDir( str ) --a function for scanning a directory and allocating it's contents to the above tables
+	local f = json.decode( str )
   	for k, v in pairs( f ) do
   		if v["type"] == "dir" and not dirIgnore[ v.name ] then
   			dirsToGet[ #dirsToGet + 1 ] = v.url
-  		elseif v["type"] == "file" and not fileIgnore[ v.name ] and v.name:match("%.(.-)$") ~= "md" then
+  		elseif v["type"] == "file" and not fileIgnore[ v.name ] and v.name:match("%.(.-)$") == "lua" then
 	 		filesToGet[ #filesToGet + 1 ] = v.download_url
 	 	end
 	end
 end
 
 local function getFile() --a function that downloads a single file at a time
-	while(#filesToGet > 0 or #dirsToGet > 0)do
+	while(isRunning or #filesToGet > 0)do
 		local sPath = table.remove( filesToGet, 1 )
 		local response
 		repeat
@@ -80,8 +84,10 @@ local function getDir() --a function that makes directories
 			response = http.get( sPath )
 		until response
 		fs.makeDir( sPath:match( "/src/(.+)" ) )
-		scanDir( response )
+		scanDir( response.readAll() )
+		response.close()
 	end
+	isRunning = false
 end
 
-parallel.waitForAll( getFile, getFile, getFile, getDir, getDir ) --runs 3 getFiles, 2 getDirs at once!
+parallel.waitForAll( getDir, getFile, getFile, getFile ) --runs 3 getFiles, 1 getDirs at once!
