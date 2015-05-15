a9b3cb24befe840cba58172f8a4991706c0833e9
KingofGamesYami <dirthsj@mcsdonline.org>
2015-05-14 19:12:07 -0500

    This works now.
    ..after several embarrassing errors

--- src/Shared/newInstaller.lua
+++ src/Shared/newInstaller.lua
@@ -1,3 +1,4 @@
+
 --this is an improved installer which will remove the need to track file paths
 --eventually it will allow updates without redownloading the entire repository
 --author: KingofGamesYami
@@ -7,87 +8,98 @@
 local json = {}
 
 do
-	local env = {} --weird meta enviorment stuffs
-	setmetatable( env, {__index = _G} )
-	local response = http.get( "https://raw.githubusercontent.com/lupus590/CC-Hive/New-installer/src/Shared/json.lua" )
-	if not response then
-		error( "Could not get json.lua", 0 )
-	end
-	local func, err = loadstring( response.readAll() )
-	setfenv( func, json )
-	if func then
-		local ok, err = pcall( func )
-		if not ok then
-			error( err, 0 )
-		end
-	else
-		error( err, 0 )
-	end
-	for k, v in pairs( env ) do
-		json[ k ] = v
-	end
+        local env = {} --weird meta enviorment stuffs
+        setmetatable( env, {__index = _G} )
+        local response = http.get( "https://raw.githubusercontent.com/lupus590/CC-Hive/New-installer/src/Shared/json.lua" )
+        if not response then
+                error( "Could not get json.lua", 0 )
+        end
+        local func, err = loadstring( response.readAll(), "json" )
+        setfenv( func, env )
+        if func then
+                local ok, err = pcall( func )
+                if not ok then
+                        error( err, 0 )
+                end
+        else
+                error( err, 0 )
+        end
+        for k, v in pairs( env ) do
+                json[ k ] = v
+        end
 end
 
-local dirIgnore = {} --dirs to ignore (blacklist)
-local fileIgnore = { --Files to ignore (blacklist)
-	["Shared/installer.lua"] = true,
-	["Shared/newInstaller.lua"] = true,
-}
+local dirsToGet = { {url="https://api.github.com/repos/lupus590/CC-Hive/contents/src/Shared", path="Shared" } }
 
-if turtle then --setting up blacklist
-	dirIgnore[ "Pocket" ] = true
-	dirIgnore[ "Client" ] = true
-	dirIgnore[ "Server" ] = true
-elseif pocket then
-	dirIgnore[ "Turtle" ] = true
-	dirIgnore[ "Server" ] = true
+if turtle then --setting up places to scan
+        table.insert( dirsToGet, {url="https://api.github.com/repos/lupus590/CC-Hive/contents/src/Turtle", path="Turtle"} )
 else
-	dirIgnore[ "Turtle" ] = true
-	dirIgnore[ "Client" ] = true
-	dirIgnore[ "Pocket" ] = true
+        table.insert( dirsToGet, {url="https://api.github.com/repos/lupus590/CC-Hive/contents/src/Client", path="Client"} )
 end
 
-local dirsToGet = {"https://api.github.com/repos/lupus590/CC-Hive/contents/src"} --a table of directories to download & scan
 local filesToGet = {} --a table of files we need to download
 local isRunning = true
 
-local function scanDir( str ) --a function for scanning a directory and allocating it's contents to the above tables
-	local f = json.decode( str )
-  	for k, v in pairs( f ) do
-  		if v["type"] == "dir" and not dirIgnore[ v.name ] then
-  			dirsToGet[ #dirsToGet + 1 ] = v.url
-  		elseif v["type"] == "file" and not fileIgnore[ v.name ] and v.name:match("%.(.-)$") == "lua" then
-	 		filesToGet[ #filesToGet + 1 ] = v.download_url
-	 	end
-	end
+local installed = {}
+
+local function scanDir( str ) --#a function for scanning a directory and allocating it's contents to the above tables
+        local f = json.decode( str )
+        for k, v in pairs( f ) do
+                if v["type"] == "dir" then
+                        table.insert( dirsToGet, {url = v.url, path = v.path:match( "src/(.+)" ) } )
+                elseif v["type"] == "file" and v.name:match("%.(.-)$") == "lua" then
+                        table.insert( filesToGet, {url = v.download_url, path = v.path:match( "src/(.+)%.lua" ) } )
+                        installed[ v.path:match( "src/(.+)%.lua" ) ] = v.sha
+                end
+        end
 end
 
 local function getFile() --a function that downloads a single file at a time
-	while(isRunning or #filesToGet > 0)do
-		local sPath = table.remove( filesToGet, 1 )
-		local response
-		repeat
-			response = http.get( sPath )
-		until response
-		local file = fs.open( sPath:match( "/src/(.+)%.$" ), "w" )
-		file.write( response.readAll() )
-		file.close()
-		response.close()
-	end
+        while isRunning or #filesToGet > 0 do
+                if #filesToGet > 0 then
+                        local toGet = table.remove( filesToGet, 1 )
+                        local r
+                        repeat
+                                print( "Downloading " .. toGet.path )
+                                r = http.get( toGet.url )
+                        until r
+                        print( "Downloaded " .. toGet.path )
+                        local file = fs.open( toGet.path, "w" )
+                        file.write( r.readAll() )
+                        file.close()
+                else
+                        sleep( 0.1 )
+                end
+        end
 end
 
 local function getDir() --a function that makes directories
-	while(#dirsToGet > 0)do
-		local sPath = table.remove( dirsToGet, 1 )
-		local response
-		repeat
-			response = http.get( sPath )
-		until response
-		fs.makeDir( sPath:match( "/src/(.+)" ) )
-		scanDir( response.readAll() )
-		response.close()
-	end
-	isRunning = false
+        while(#dirsToGet > 0)do
+                local sPath = table.remove( dirsToGet, 1 )
+                local response
+                repeat
+                        response = http.get( sPath.url )
+                until response
+                fs.makeDir( sPath.path )
+                scanDir( response.readAll() )
+                response.close()
+        end
+        isRunning = false
 end
 
-parallel.waitForAll( getDir, getFile, getFile, getFile ) --runs 3 getFiles, 1 getDirs at once!
+local function installDefault()
+  parallel.waitForAll( getDir, getFile, getFile, getFile ) --runs 3 getFiles, 1 getDirs at once!
+  print( "Done!" )
+  local file = fs.open( "installed", "w" )
+  for k, v in pairs( installed ) do
+    file.writeLine( k .. " : " .. v )
+  end
+  file.close()
+end
+
+local tArgs = {...}
+if #tArgs == 0 then
+  installDefault()
+else
+  --#todo: packages
+end
